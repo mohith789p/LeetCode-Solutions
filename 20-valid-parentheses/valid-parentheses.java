50e6aa5341ebeab9bff10dae98c72da07354ca81
Potnuru Mohith <mohith321p@gmail.com>
2026-02-08 19:10:35 +0530

    Time: 3 ms (87.67%) | Memory: 43.2 MB (71.53%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,28 +1,29 @@
 class Solution {
-    static Map<Character , Character> mp;
-    static {
-        mp = new HashMap<>();
-        mp.put('{','}');
-        mp.put('(',')');
-        mp.put('[',']');
-    }
+    public boolean isValid(String s) {
+        Stack<Character> sk = new Stack<>();
 
-    public boolean isValid(String str) {
-        Stack<Character> s = new Stack();
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (ch == '(' || ch == '{' || ch == '[') {
-                s.push(ch);
-            } else if (!s.isEmpty()){
-                char c = s.pop();
-                if (mp.get(c) != ch) {
+        for(char ch : s.toCharArray()){
+            if(ch == '{' || ch == '[' || ch == '('){
+                sk.push(ch);
+            }
+
+            else {
+                if (sk.isEmpty())
                     return false;
-                }
-            } else {
-                return false;
+                char top = sk.pop();
+
+                if(ch == ')' && top != '(')
+                    return false;
+
+                if(ch == ']' && top != '[')
+                    return false;
+
+                if(ch == '}' && top != '{')
+                    return false;
+        
             }
         }
 
-        return s.isEmpty();
+        return sk.isEmpty();
     }
 }
