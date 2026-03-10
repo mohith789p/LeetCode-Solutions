4ce52547cfbf0d0dd8e26c2efca4f11613b7cd1e
Potnuru Mohith <mohith321p@gmail.com>
2026-03-10 14:02:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (14.30%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 2) return 2;
+        if(n == 1) return 1;
+
+        int a = 1, b = 2, c = 0;
+        for(int i = 3; i <= n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
