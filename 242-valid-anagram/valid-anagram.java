d2cbe2250608520c573123d05f88b061d0e22a40
Potnuru Mohith <mohith321p@gmail.com>
2026-03-28 22:15:37 +0530

    Time: 17 ms (16.29%) | Memory: 46.6 MB (20.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,15 +2,18 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         Map<Character, Integer> mp = new HashMap<>();
 
+        if(s.length() != t.length()) return false;
+
         for(char ch : s.toCharArray()){
             mp.put(ch, mp.getOrDefault(ch,0) + 1);
         }
 
         for(char ch : t.toCharArray()){
-            mp.put(ch, mp.getOrDefault(ch, 0) - 1);
-            if(mp.get(ch) == 0) mp.remove(ch);
+            if(!mp.containsKey(ch) || mp.get(ch) == 0)
+                return false;
+            mp.put(ch, mp.get(ch) - 1);
         }
 
-        return mp.size() == 0;
+        return true;
     }
 }
