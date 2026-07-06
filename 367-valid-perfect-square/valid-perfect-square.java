994411bebc01cacc65a160667a222a84e5d3da9f
Potnuru Mohith <mohith321p@gmail.com>
2026-07-06 23:30:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (37.72%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -7,7 +7,6 @@ class Solution {
         while(low <= high){
             int mid = low + (high - low) / 2;
             double res = (double) num / mid;
-            System.out.println(res);
             if(mid == res) return true;
 
             else if(mid > res) high = mid - 1;
