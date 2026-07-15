9a38aa6dbc8eab09554018a1c105cc98d1a858eb
Potnuru Mohith <mohith321p@gmail.com>
2026-07-15 11:22:13 +0530

    Time: 2 ms (96.62%) | Memory: 45.4 MB (92.11%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -3,22 +3,22 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> sub = new ArrayList<>();
 
-        back(candidates, target, 0, 0, sub, ans);
+        backtrack(candidates, target, 0, sub, ans);
         return ans;
     }
 
-    private void back(int[] arr, int k, int i, int sum, List<Integer> sub, List<List<Integer>> res){
-        if(sum == k){
+    private void backtrack(int[] arr, int t, int i, List<Integer> sub, List<List<Integer>> res){
+        if(t == 0) {
             res.add(new ArrayList<>(sub));
             return;
         }
 
+        if(t < 0 || i == arr.length) return;
+        
         for(int j = i; j < arr.length; j++){
-            if(sum > k) continue;
-
             sub.add(arr[j]);
-            back(arr, k, j, sum + arr[j], sub, res);
-            sub.remove(sub.size() -1);
+            backtrack(arr, t - arr[j], j, sub, res);
+            sub.remove(sub.size() - 1);
         }
     }
 }
