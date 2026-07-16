18e81763fdc08a51ff7256c26f8ccfdf016cc601
Potnuru Mohith <mohith321p@gmail.com>
2026-07-16 07:54:22 +0530

    Time: 5 ms (83.24%) | Memory: 46.7 MB (81.17%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,50 @@
+class Graph {
+    List<List<Integer>> adj = new ArrayList<>();
+
+    Graph(int n) {
+        while (n-- != 0) {
+            adj.add(new ArrayList<>());
+        }
+    }
+
+    void addEdge(int u, int v) {
+        adj.get(u).add(v);
+    }
+
+    List<Integer> getNeigh(int curr) {
+        return adj.get(curr);
+    }
+}
+
+class Solution {
+    public int[] findOrder(int n, int[][] edges) {
+        int[] indeg = new int[n];
+        Graph gh = new Graph(n);
+
+        for (int[] edge : edges) {
+            indeg[edge[0]]++;
+            gh.addEdge(edge[1], edge[0]);
+        }
+
+        int idx = 0;
+        int[] ans = new int[n];
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (indeg[i] == 0)
+                q.add(i);
+        }
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            ans[idx++] = cur;
+            for (int neigh : gh.getNeigh(cur)) {
+                indeg[neigh]--;
+                if (indeg[neigh] == 0)
+                    q.add(neigh);
+            }
+        }
+        
+        return idx != n ? new int[0] : ans;
+    }
+}
