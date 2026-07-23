93430ec2d22b051bc93a5c54191c7a161af9a58d
Potnuru Mohith <mohith321p@gmail.com>
2026-07-23 09:09:38 +0530

    Time: 294 ms (39.84%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
 Select Max(salary) as SecondHighestSalary
 from employee
-where salary < (Select Max(salary) as SecondHighestSalary
-from employee)
+where salary not in (select max(salary) from Employee);
